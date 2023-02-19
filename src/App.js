cb05ca652d7fc03ce54598cd6514406c063a17a6
Marek Juričko <marek.juricko@azet.sk>
2023-02-19 20:31:07 +0100

    created Lorem generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data"
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [paragraphs, setParagraphs] = useState([])
+
+  const formSubmit = (event) => {
+    event.preventDefault()
+    let amount = parseInt(count)
+
+    //if the amount number is higher or lover than max/min, it is set to max/min value
+    if(amount <= -1){
+      amount = 1
+    }else if(amount >= 6){
+      amount = 5
+    }
+
+    //returns paragraphs based on amount number
+    setParagraphs(data.slice(0, amount))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-section">
+      <h1>Lorem ipsum generator</h1>
+      <form className="form-only-section" onSubmit={formSubmit}>
+        <input type="number" placeholder="number of paragraphs" value={count} onChange={(event) => {setCount(event.target.value)}}/>
+        <input type="submit" value="Generate"/>
+      </form>
+
+      {paragraphs.map((oneParagraph, index) => {
+        return <p className="item" key={index}>{oneParagraph}</p>
+      })}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
